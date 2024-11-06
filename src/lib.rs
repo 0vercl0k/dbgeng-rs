8ef556781048d59727375c6a9d970419809a99d3
wumb0 <wumb0@finishyour.beer>
2024-11-06 00:33:28 -0500

    Synthetic module support (#4)
    Co-authored-by: 0vercl0k <1476421+0vercl0k@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,9 @@
 pub mod as_pcstr;
 pub mod bits;
 pub mod client;
+
+#[allow(non_snake_case)]
+#[inline(always)]
+pub fn DEBUG_EXTENSION_VERSION(major: u32, minor: u32) -> u32 {
+    ((major & 0xff_ff) << 16) | (minor & 0xff_ff)
+}
